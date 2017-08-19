173aa3b601adebf6fc10ce64898187abbce1934c
markandersonus <mark_anderson_us@yahoo.com>
2017-08-19 08:40:41 -0500

    manytomany complete - lecture 97

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,62 @@
+package guru.springframework.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.*;
+
+@Entity
+public class Ingredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String description;
+	private BigDecimal amount;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
+	
+	@ManyToOne
+	private Recipe recipe;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
+	}
+	
+}
---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,34 @@
+package guru.springframework.domain;
+
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+	
+	@OneToOne
+	private Recipe recipe;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String uom) {
+		this.description = uom;
+	}
+
+	public Recipe getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(Recipe recipe) {
+		this.recipe = recipe;
+	}
+	
+}
